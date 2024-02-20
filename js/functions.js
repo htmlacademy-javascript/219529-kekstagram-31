a6bbe1c82dc8fa4d26cfe0f4baf98d24c296f6f5
marusyaby <mariakhrol@gmail.com>
2024-02-20 13:55:12 +0300

    Add checkStringLength, checkPalindrome, getNumber functions

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+const checkStringLength = (str, maxLength) =>
+  str.length <= maxLength;
+
+const checkPalindrome = (str) => {
+  const spacelessStr = str.toLowerCase().replaceAll(' ','');
+  const reversedStr = spacelessStr.split('').reverse().join('');
+  return spacelessStr === reversedStr;
+};
+
+const getNumber = (str) => {
+  const strNumbers = str.toString().match(/[0-9]/g);
+  return (strNumbers) ? Number(strNumbers.join('')) : NaN;
+};

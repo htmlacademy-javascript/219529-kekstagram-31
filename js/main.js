fa461d3decc9456c125b7792f9d6d1a169c8e6f3
marusyaby <mariakhrol@gmail.com>
2024-03-24 22:40:03 +0300

    Rename createData functions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-import {createPhotos} from './data.js';
+import {createDataPhotos} from './data.js';
 import {PHOTOS_MAX} from './const.js';
 import {renderThumbnails} from './render.js';
 
-const dataPhotos = createPhotos(PHOTOS_MAX);
+const dataPhotos = createDataPhotos(PHOTOS_MAX);
 renderThumbnails(dataPhotos);

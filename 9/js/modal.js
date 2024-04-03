867624ad21ca5b6fdec26e2547a28b97704d92cd
Keks <keks@htmlacademy.ru>
2024-04-03 19:13:34 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/modal.js
+++ 9/js/modal.js
@@ -1,14 +1,37 @@
 const openModal = (modalElement) => {
+  const closeButtonElement = modalElement.querySelector('.cancel');
   modalElement.classList.remove('hidden');
+  modalElement.classList.add('modal');
   document.body.classList.add('modal-open');
+  document.addEventListener('keydown', documentKeydownHandler);
+  closeButtonElement.addEventListener('click', closeButtonClickHandler);
 };
 
 const closeModal = (modalElement) => {
+  const closeButtonElement = modalElement.querySelector('.cancel');
   modalElement.classList.add('hidden');
+  modalElement.classList.remove('modal');
   document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', documentKeydownHandler);
+  closeButtonElement.removeEventListener('click', closeButtonClickHandler);
 };
 
-export {
-  openModal,
-  closeModal,
-};
+function documentKeydownHandler (evt) {
+  if (evt.key === 'Escape'
+    && !evt.target.classList.contains('text__hashtags')
+    && !evt.target.classList.contains('text__description')) {
+    evt.preventDefault();
+    const modalElement = document.querySelector('.modal');
+    if (modalElement) {
+      closeModal(modalElement);
+    }
+  }
+}
+
+function closeButtonClickHandler (evt) {
+  evt.preventDefault();
+  const modalElement = document.querySelector('.modal');
+  closeModal(modalElement);
+}
+
+export {openModal};
